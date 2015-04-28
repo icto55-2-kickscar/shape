fd5ddfe04f68185f88a2033de580f4fea34fae73
안대혁 <kickscar@sunnyvale.co.kr>
2015-04-29 01:10:52 +0900

    init commit

---
+++ src/shape/ShapeTest.java
@@ -0,0 +1,15 @@
+package shape;
+
+public class ShapeTest {
+
+	public static void main(String[] args) {
+		
+		Point point1 = new Point();
+		
+		point1.setX(2);
+		point1.setX(5);
+		point1.show();
+				
+	}
+
+}
